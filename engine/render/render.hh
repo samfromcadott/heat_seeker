d1bfad36dc89ae012313a5c15a9bd73a4876bfda
samfromcadott <samahstan@gmail.com>
2025-11-16 13:17:30 -0600

    Created minimal SDL program

---
+++ engine/render/render.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace HS {
+
+class Render {
+private:
+
+public:
+	Render();
+	~Render();
+};
+
+}
---
+++ engine/render/systems.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace HS {
+
+void render_models(); ///< Renders non-BVH models
+void render_bvh_models(); ///< Renders BVH models
+void update_armature(); ///< Advances the animation armatures of models
+
+}
