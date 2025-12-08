58595abb3d6b9c44aa4268a785b2002a5e30539e
samfromcadott <samahstan@gmail.com>
2025-12-08 09:37:15 -0600

    Made a test for interaction between bodies

--- engine/src/main.cc
+++ engine/src/main.cc
@@ -16,6 +16,7 @@ int main() {
 	Game.component<PhysicsEngine>().add(flecs::Singleton);
 	Game.set<PhysicsEngine>( PhysicsEngine() );
 	Game.get_mut<PhysicsEngine>().set_world(Game);
+	Game.get_mut<PhysicsEngine>().set_gravity( glm::vec3(0.0, -9.8, 0.0) );
 
 	// Register systems
 	Game.system<Body&, Position&>().each(pos_to_body);
@@ -35,7 +36,7 @@ int main() {
 	e1.add<HSE::Model>();
 	e1.add<Position>();
 	e1.add<Rotation>();
-	e1.get_mut<Position>() = glm::vec3(1.0,0.0,3.0);
+	e1.get_mut<Position>() = glm::vec3(0.5,5.0,0.0);
 	e1.add<Body>();
 	e1.set<Body>( Body(Game.get_mut<PhysicsEngine>(), e1, JPH::BodyCreationSettings(
 		new JPH::SphereShape(1.0),
@@ -49,8 +50,16 @@ int main() {
 	e2.add<HSE::Model>();
 	e2.add<Position>();
 	e2.add<Rotation>();
-	e2.get_mut<Position>() = glm::vec3(-1.0,2.0,-5.0);
+	e2.get_mut<Position>() = glm::vec3(0.0,0.0,0.0);
 	e2.get_mut<Rotation>() = glm::quat(0.9238795, 0, 0.3826834, 0);
+	e2.add<Body>();
+	e2.set<Body>( Body(Game.get_mut<PhysicsEngine>(), e1, JPH::BodyCreationSettings(
+		new JPH::SphereShape(1.0),
+		JPH::RVec3::sZero(),
+		JPH::Quat::sIdentity(),
+		JPH::EMotionType::Static,
+		Layers::NON_MOVING
+	)));
 
 	// Set their models
 	::Model sphere = LoadModelFromMesh( GenMeshSphere(1.0, 4, 8) );
