a5cc4526621c21b4b5c9d42e7cdf6094fe66d15a
samfromcadott <samahstan@gmail.com>
2025-12-11 10:51:33 -0600

    Added character systems

--- engine/include/HSE/physics_systems.hh
+++ engine/include/HSE/physics_systems.hh
@@ -4,10 +4,20 @@ namespace HSE {
 
 void pos_to_body(Body& b, Position& p);
 void rot_to_body(Body& b, Rotation& r);
-void vel_to_body(Body& b, Velocity& r);
+void vel_to_body(Body& b, Velocity& v);
+
+void pos_to_character(CharacterBody& b, Position& p);
+void rot_to_character(CharacterBody& b, Rotation& r);
+void vel_to_character(CharacterBody& b, Velocity& v);
+
 void physics_update(PhysicsEngine& pe);
+
 void body_to_pos(Body& b, Position& p);
 void body_to_rot(Body& b, Rotation& r);
-void body_to_vel(Body& b, Velocity& r);
+void body_to_vel(Body& b, Velocity& v);
+
+void character_to_pos(CharacterBody& b, Position& p);
+void character_to_rot(CharacterBody& b, Rotation& r);
+void character_to_vel(CharacterBody& b, Velocity& v);
 
 }
--- engine/src/physics/systems.cc
+++ engine/src/physics/systems.cc
@@ -17,6 +17,21 @@ void HSE::vel_to_body(Body& b, Velocity& v) {
 		b.set_velocity(v);
 }
 
+void HSE::pos_to_character(CharacterBody& b, Position& p) {
+	if ( b.get_position() != glm::vec3(p) )
+		b.set_position(p);
+}
+
+void HSE::rot_to_character(CharacterBody& b, Rotation& r) {
+	if ( b.get_rotation() != glm::quat(r) )
+		b.set_rotation(r);
+}
+
+void HSE::vel_to_character(CharacterBody& b, Velocity& v) {
+	if ( b.get_velocity() != glm::vec3(v) )
+		b.set_velocity(v);
+}
+
 void HSE::physics_update(PhysicsEngine& pe) {
 	pe.update();
 }
@@ -30,5 +45,17 @@ void HSE::body_to_rot(Body& b, Rotation& r) {
 }
 
 void HSE::body_to_vel(Body& b, Velocity& v) {
+	v = b.get_velocity();
+}
+
+void HSE::character_to_pos(CharacterBody& b, Position& p) {
+	p = b.get_position();
+}
+
+void HSE::character_to_rot(CharacterBody& b, Rotation& r) {
+	r = b.get_rotation();
+}
+
+void HSE::character_to_vel(CharacterBody& b, Velocity& v) {
 	v = b.get_velocity();
 }
