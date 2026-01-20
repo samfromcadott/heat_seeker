cf78b1de968b91c92d2f4d83a403035a8fc077e4
samfromcadott <samahstan@gmail.com>
2026-01-20 10:02:35 -0600

    Fixed anmbiguous naming in `render_models`

--- engine/src/render/models.cc
+++ engine/src/render/models.cc
@@ -1,12 +1,14 @@
 #include <heat_seeker.hh>
 
+using namespace HSE;
+
 HSE::Model::Model(const std::string& filename) {
 	if ( HSE::model_files.count(filename) == 0 ) HSE::model_files[filename].model = LoadModel( filename.c_str() );
 	data = &HSE::model_files[filename];
 }
 
 void HSE::render_models(HSE::Model& m, HSE::Position& p, HSE::Rotation& r) {
-	glm::vec3 axis = glm::axis( glm::quat(r) );
-	float angle = glm::angle( glm::quat(r) ) * 180.0 / PI;
-	DrawModelWiresEx(m.data->model, p, Vector3 {axis.x,axis.y,axis.z}, angle, {1.0,1.0,1.0}, BLACK);
+	vec3 ax = axis( quat(r) );
+	float ang = angle( quat(r) ) * 180.0 / PI;
+	DrawModelWiresEx(m.data->model, p, Vector3 {ax.x,ax.y,ax.z}, ang, {1.0,1.0,1.0}, BLACK);
 }
