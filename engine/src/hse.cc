09bbdda9896bb044916c7d5e47a68fb6a17203db
samfromcadott <samahstan@gmail.com>
2026-01-26 10:06:33 -0600

    Added file with init and quit

---
+++ engine/src/hse.cc
@@ -0,0 +1,10 @@
+#include <heat_seeker.hh>
+
+void HSE::init(const std::string& title, const int width, const int height) {
+	InitWindow( width, height, title.c_str() );
+	SetTargetFPS(60);
+}
+
+void HSE::quit() {
+	CloseWindow();
+}
