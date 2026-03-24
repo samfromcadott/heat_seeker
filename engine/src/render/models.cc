3ee4f07ae06b3aa6ad4f38781268df691d07ebf9
samfromcadott <samahstan@gmail.com>
2026-03-23 19:10:10 -0500

    Models are rendered with their texture

--- engine/src/render/models.cc
+++ engine/src/render/models.cc
@@ -12,7 +12,7 @@ HSE::Model::Model(const std::string& filename) {
 	// Set the shader for the model
 	for (int i = 0; i < HSE::model_files[filename].model.materialCount; i++) {
 		HSE::model_files[filename].model.materials[i].shader = gouraud_shader;
-		HSE::model_files[filename].model.materials[i].maps[MATERIAL_MAP_DIFFUSE].texture = uv_debug_texture;
+		// HSE::model_files[filename].model.materials[i].maps[MATERIAL_MAP_DIFFUSE].texture = uv_debug_texture;
 	}
 
 	data = &HSE::model_files[filename];
@@ -26,7 +26,8 @@ void HSE::render_models(HSE::Model& m, HSE::Position& p, HSE::Rotation& r) {
 	if (m.data->animation_count > 0)
 		UpdateModelAnimation(m.data->model, m.data->animations[m.animation], m.frame);
 
-	DrawModelEx(m.data->model, p, Vector3 {ax.x,ax.y,ax.z}, ang, {1.0,1.0,1.0}, m.debug_color);
+	// DrawModelEx(m.data->model, p, Vector3 {ax.x,ax.y,ax.z}, ang, {1.0,1.0,1.0}, m.debug_color);
+	DrawModelEx(m.data->model, p, Vector3 {ax.x,ax.y,ax.z}, ang, {1.0,1.0,1.0}, WHITE);
 }
 
 void HSE::update_animation(HSE::Model& m) {
