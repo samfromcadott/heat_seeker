575af29b1c98b9351e49faf709c1944a6b4cb0b8
samfromcadott <samahstan@gmail.com>
2026-09-17 15:24:28 -0500

    `explode` now uses center of mass

--- rpg/src/weapon.cc
+++ rpg/src/weapon.cc
@@ -144,12 +144,15 @@ void explode(Entity entity) {
 		float dist = distance( vec3(get<Position>(entity)), vec3(get<Position>(e)) );
 		float coef = std::lerp(1.0, 0.0, dist/radius);
 		coef = std::clamp(coef, 0.0f, 1.0f);
-		vec3 dir = vec3(get<Position>(e)) - vec3(get<Position>(entity));
 
-		if ( has<Body>(e) )
+		if ( has<Body>(e) ) {
+			vec3 dir = get<Body>(e).get_center() - vec3(get<Position>(entity));
 			get<Body>(e).add_force(force * dir * coef);
-		else if ( has<CharacterBody>(e) )
+		}
+		else if ( has<CharacterBody>(e) ) {
+			vec3 dir = get<CharacterBody>(e).get_center() - vec3(get<Position>(entity));
 			get<Velocity>(e) = vec3(get<Velocity>(e)) + force * dir * coef * 0.05f;
+		}
 
 		if ( has<Health>(e) )
 			get<Health>(e).now -= damage*coef;
